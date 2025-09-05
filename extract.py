124307b3ce3b59f606f4a45c0915d06d005955bf
Miko Osak <mikodoesminecraft@gmail.com>
2025-09-05 10:42:05 +0100

    Fix some issues with extraction if no newline

--- extract.py
+++ extract.py
@@ -9,14 +9,14 @@ jobs = pd.read_csv(sys.argv[1])
 number_of_jobs = len(jobs)
 print(f"Processing {number_of_jobs} entries...")
 counts = dict()
-blacklist = ["SIEM", "NIST", "HMRC", "GDPR", "FTSE", "MHCLG", "CSOC", "STEM", "CISO", "EUSS", "ISMS", "DBSC", "ASOS", "NCSC", "DORA", "STRAP", "CREST", "KINTO", "UBDS", "GIAC", "KPMG", "CHECK", "IDEAL", "WILL", "FULL", "COST", "CEMA", "SAST", "DAST", "SANS", "FREE", "MITRE", "OWASP", "CSPM", "EMSOU", "CSIRT", "DFIR", "MSSP", "DHCP", "JCKP", "IASME", "ITIL", "ITSM", "CE+", "CODE", "PHIA", "CERT", "REST", "SOAP", "LGBTQ", "TPRM", "SOAR", "CFGI", "OSINT", "RAND", "GCSE", "TPDD", "EMEA", "ASDA", "APPLY", "NATO", "FMLA", "USAF", "MADSS", "HIPAA", "EPPA", "AFMAN", "AFIN", "STEP", "HBSS", "DISA", "STIG", "CSSP", "MACCE", "HTTP", "NOTE", "ACAS", "CMMC", "NERC", "MUST", "USMC", "RKON", "SPAC", "SMADS", "CJIS", "MAJOR", "CCPA", "CIAM", "CNSSI", "PGTEK", "FISMA", "MTAC", "WITH", "DFAS", "AFSIM", "LEVEL", "SAML", "COBIT", "ISACA", "SCADA", "HIDS", "EQUAL", "JANUS", "JWICS", "ISSO", "IAVM", "DFARS", "NIPR", "FAIR", "TVFCU", "SCAP", "SDLC", "KAIT", "BAIT", "ESMA", "ITAR", "SAIC", "ISSM"]
+blacklist = ["SIEM", "NIST", "HMRC", "GDPR", "FTSE", "MHCLG", "CSOC", "STEM", "CISO", "EUSS", "ISMS", "DBSC", "ASOS", "NCSC", "DORA", "STRAP", "CREST", "KINTO", "UBDS", "GIAC", "KPMG", "CHECK", "IDEAL", "WILL", "FULL", "COST", "CEMA", "SAST", "DAST", "SANS", "FREE", "MITRE", "OWASP", "CSPM", "EMSOU", "CSIRT", "DFIR", "MSSP", "DHCP", "JCKP", "IASME", "ITIL", "ITSM", "CE+", "CODE", "PHIA", "CERT", "REST", "SOAP", "LGBTQ", "TPRM", "SOAR", "CFGI", "OSINT", "RAND", "GCSE", "TPDD", "EMEA", "ASDA", "APPLY", "NATO", "FMLA", "USAF", "MADSS", "HIPAA", "EPPA", "AFMAN", "AFIN", "STEP", "HBSS", "DISA", "STIG", "CSSP", "MACCE", "HTTP", "NOTE", "ACAS", "CMMC", "NERC", "MUST", "USMC", "RKON", "SPAC", "SMADS", "CJIS", "MAJOR", "CCPA", "CIAM", "CNSSI", "PGTEK", "FISMA", "MTAC", "WITH", "DFAS", "AFSIM", "LEVEL", "SAML", "COBIT", "ISACA", "SCADA", "HIDS", "EQUAL", "JANUS", "JWICS", "ISSO", "IAVM", "DFARS", "NIPR", "FAIR", "TVFCU", "SCAP", "SDLC", "KAIT", "BAIT", "ESMA", "ITAR", "SAIC", "ISSM", "eMASS"]
 
 def add_count(to_add, dictionary):
    dictionary[to_add] = dictionary.get(to_add, 0) + 1
 
 def extract_and_count(row):
     
-    to_match = r"\b[A-Z]{4,5}\b|\b[A-Za-z]+?\+|CEH|\b[A-Z]{2}\b-\b[0-9]{3}\b|CCE|CAP|EnCE|CFR|GSE|CRT"
+    to_match = r"\b[A-Z]{4,5}\b|(?<=[a-z])[A-Z]{4,5}|[A-Z][a-z]+?\+|\b[A-Z]{2}\b-\b[0-9]{3}\b|\be[A-Z].*?\b|CEH|CCE|CAP|EnCE|CFR|GSE|CRT"
     found = re.findall(to_match, str(row["text"]))
 
     for i in found:
@@ -31,9 +31,9 @@ for i in blacklist:
     counts.pop(i, None)
 
 to_clean = []
-for key, val in counts.items():
-    if val < number_of_jobs / 100:
-        to_clean.append(key)
+#for key, val in counts.items():
+#    if val < number_of_jobs / 100:
+#        to_clean.append(key)
 
 for i in to_clean:
     counts.pop(i, None)
